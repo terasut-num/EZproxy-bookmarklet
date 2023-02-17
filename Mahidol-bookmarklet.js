1ead31b52ee752294c576165ce0931845c7c7a9f
terasut-num <terasut.num@alumni.mahidol.ac.th>
2023-02-17 23:02:33 +0700

    Update Mahidol-bookmarklet.js
    Javascript for accessing the Mahidol E-database

--- Mahidol-bookmarklet.js
+++ Mahidol-bookmarklet.js
@@ -1,1 +1,6 @@
+// Open in New Tab
 javascript:window.open('https://ejournal.mahidol.ac.th/login?url='+location.href).focus();
+
+
+// Open in Old Tab
+javascript:void(location.href='https://ejournal.mahidol.ac.th/login?url='+location.href);
