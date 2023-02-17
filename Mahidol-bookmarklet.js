b9c181abcd16290aec6ae85a68e5fae8c316b876
terasut-num <118581170+terasut-num@users.noreply.github.com>
2023-02-17 23:50:54 +0700

    Update Mahidol-bookmarklet.js

--- Mahidol-bookmarklet.js
+++ Mahidol-bookmarklet.js
@@ -1,4 +1,5 @@
-// Choose only one of these two codes
+// Just pick one of these two codes
+
 // Open in New Tab
 javascript:window.open('https://ejournal.mahidol.ac.th/login?url='+location.href).focus();
 
